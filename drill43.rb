ba30797f82cc1bef925857f6e197f9c3086557ca
kazu7719 <kazuki.eripon7719@icloud.com>
2025-10-24 15:41:46 +0900

    ruby ドリル問題43

---
+++ drill43.rb
@@ -0,0 +1,11 @@
+class Book
+  attr_reader :title, :price
+  def initialize(title, price)
+    @title = title
+    @price = price
+  end 
+end
+
+book = Book.new("嫌われる勇気", "1,450")
+puts book.title
+puts "#{book.price}円"
